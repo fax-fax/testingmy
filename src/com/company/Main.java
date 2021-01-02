84c9b3b0cb3ef6d6bceb1328cbc5982cb88d0e3c
Your Name <you@example.com>
2021-01-02 22:34:13 +0300

    перед вторым push в git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 //HelloWorld
     public static void main(String[] args) {
 	// write your code here
-    System.out.println("Hello");
+    System.out.println("Hello1");
+    System.out.println("Hello2");
     }
 }
