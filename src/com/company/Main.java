a9cce3544dcbad1c57004a628b3e0438f409e32a
Sergik <Sergik@Sergik.ru>
2021-01-28 15:42:33 +0300

    Начало работы

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+//import SocketServer;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Start/RU: Старт");
+        //создаем объект класса SocketServer
+        SocketServer ss= new SocketServer();
+        ss.main(args);
+    }
+}
