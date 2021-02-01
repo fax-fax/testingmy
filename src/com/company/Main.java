87cf9f0f181cd964d3ad05f7ee621b7a0c6aa9e8
Sergik <Sergik@Sergik.ru>
2021-02-01 10:37:13 +0300

    01.02.21 Ветка обучения 1. Новый класс DebuggingList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,9 @@ public class Main {
 	// write your code here
         System.out.println("Start/RU: Старт");
         //создаем объект класса SocketServer
-        SocketServer ss= new SocketServer();
-        ss.main(args);
+       // SocketServer ss= new SocketServer();
+       // ss.main(args);
+//01.02.21 Ветка обучения 1
+        System.out.println("Start/RU: Старт. 01.02.21");
     }
 }
